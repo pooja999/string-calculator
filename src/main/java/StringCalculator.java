025b96ff96b58dbf6af03718bf945758adca925b
pooja <shekar.pooh@gmail.com>
2020-10-27 11:55:26 +0530

    make the failing test pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,6 +3,10 @@ public class StringCalculator {
         if(input.isEmpty()) {
             return 0;
         }
+        if(input.contains(",")) {
+            String[] values = input.split(",");
+            return Integer.parseInt(values[0])+Integer.parseInt(values[1]);
+        }
         return Integer.parseInt(input);
     }
 }
