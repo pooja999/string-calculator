764fd95d8a9959d550d7b8fbcfdcda9777de4cce
pooja <shekar.pooh@gmail.com>
2020-10-27 08:51:59 +0530

    add a failing test

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -12,4 +12,9 @@ public class StringCalculatorTest {
     public void shouldReturn1For1() {
         Assert.assertEquals(1,StringCalculator.add("1"));
     }
+
+    @Test
+    public void shouldReturn2For2() {
+        Assert.assertEquals(2,StringCalculator.add("2"));
+    }
 }
