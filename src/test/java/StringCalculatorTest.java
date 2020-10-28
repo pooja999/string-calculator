b2c4d2cdafbb561f7dcbc0419a673bc13c345827
pooja <shekar.pooh@gmail.com>
2020-10-28 10:03:35 +0530

    add a failing testcase for number greater than 1000

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -11,23 +11,23 @@ public class StringCalculatorTest {
 
     @Test
     public void shouldReturnValueForSingleNumber() throws Exception {
-        Assert.assertEquals(1,StringCalculator.add("1"));
-        Assert.assertEquals(2,StringCalculator.add("2"));
-        Assert.assertEquals(10,StringCalculator.add("10"));
+        Assert.assertEquals(1, StringCalculator.add("1"));
+        Assert.assertEquals(2, StringCalculator.add("2"));
+        Assert.assertEquals(10, StringCalculator.add("10"));
     }
 
     @Test
     public void shouldReturnSumForAnyAmountOfNumbersSeparatedByNewLineOrComma() throws Exception {
-        Assert.assertEquals(3,StringCalculator.add("1,2"));
-        Assert.assertEquals(6,StringCalculator.add("1,2,3"));
-        Assert.assertEquals(3,StringCalculator.add("1\n2"));
-        Assert.assertEquals(6,StringCalculator.add("1\n2,3"));
+        Assert.assertEquals(3, StringCalculator.add("1,2"));
+        Assert.assertEquals(6, StringCalculator.add("1,2,3"));
+        Assert.assertEquals(3, StringCalculator.add("1\n2"));
+        Assert.assertEquals(6, StringCalculator.add("1\n2,3"));
     }
 
     @Test
     public void shouldReturnSumForChangedDelimiter() throws Exception {
-        Assert.assertEquals(1,StringCalculator.add("//;\n1"));
-        Assert.assertEquals(3,StringCalculator.add("//;\n1;2"));
+        Assert.assertEquals(1, StringCalculator.add("//;\n1"));
+        Assert.assertEquals(3, StringCalculator.add("//;\n1;2"));
     }
 
     @Test
@@ -40,5 +40,10 @@ public class StringCalculatorTest {
     public void shouldThrowAnExceptionForANegativeNumberInASequence() {
         Assertions.assertThatThrownBy(() -> StringCalculator.add("-1,2")).hasMessage("negatives not allowed: -1");
         Assertions.assertThatThrownBy(() -> StringCalculator.add("//;\n1;-2")).hasMessage("negatives not allowed: -2");
+    }
+
+    @Test
+    public void shouldReturn0ForNumberGreaterThan1000() throws Exception {
+        Assert.assertEquals(0, StringCalculator.add("1001"));
     }
 }
